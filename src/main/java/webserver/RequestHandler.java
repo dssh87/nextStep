a890a4b6436f40c18a39994417caf096b256290e
dssh87 <dssh87@gmail.com>
2020-07-14 00:11:40 +0900

    4장까지 완료(302 status code)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,14 +2,21 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,18 +41,50 @@ public class RequestHandler extends Thread {
         		return;
         	}
         	
+        	String[] tokens = line.split(" "); 
+        	int contentLength = 0;
         	while(!line.equals("")){
-        		line = br.readLine();
         		log.debug("header : {}", line);
+        		line = br.readLine();
+        		if(line.contains("Content-Length")) {
+        			contentLength = getContentLength(line);
+        		}
         	}
         	
-        	DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Animal World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        	String url = tokens[1];
+        	log.debug("url ... "+url);
+        	if(("/user/create").equals(url)) {
+        		String body = IOUtils.readData(br, contentLength);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		log.debug("User : {}", user);
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos, "/index.html");
+        		
+        	}else {
+        		DataOutputStream dos = new DataOutputStream(out);
+        		byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+        		response200Header(dos, body.length);
+        		responseBody(dos, body);
+        	}
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private int getContentLength(String line) {
+    	String[] headerTokens = line.split(":");
+    	return Integer.parseInt(headerTokens[1].trim());
+    }
+    
+    private void response302Header(DataOutputStream dos, String url) {
+    	try {
+    		dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+    		dos.writeBytes("Location: "+ url + "\r\n");
+    		dos.writeBytes("\r\n");
+    	}catch(IOException e) {
+    		log.error(e.getMessage());
+    	}
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
